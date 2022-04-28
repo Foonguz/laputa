3e5916ddf1db05fd086609aa683638ab7fe1bda2
Foonguz <93399413+Foonguz@users.noreply.github.com>
2022-04-28 10:15:13 +0200

    Some changes

--- OverWorld.py
+++ OverWorld.py
@@ -7,14 +7,16 @@ class Player:
     #[True, True, 0] stands for ['Going up', 'Going down' and 'posistion']
     X = [True, True, 0]
     Y = [True, True, 0]
+    isMade = False
     
-    def __init__(self, X, Y):
+    def __init__(self, X, Y, isMade):
         self.X = X
         self.Y = Y
+        self.isMade = isMade
         
 
 #two different player classes
-playerOver = Player([False, False, 362], [False, False, 268])
+playerOver = Player([False, False, 362], [False, False, 268], False)
     
 class Enemy:
     X = 0
@@ -26,28 +28,44 @@ class Enemy:
         self.Y = Y
         self.isMade = isMade
 
+class PlayMode:
+    inPlayMode = False
+    groundIs = [0, 0]
+
 class Lay(): 
     layout = ['']
 
 RandomGeneration(Lay.layout)
 
-testEnemy = Enemy(random.randrange(20), random.randrange(20), False)
+testEnemy = Enemy(random.randrange(19), random.randrange(19), False)
+
 while(testEnemy.isMade == False):
     if(testEnemy.X*20 + testEnemy.Y <= 400):
         if(Lay.layout[0][testEnemy.X*20 + testEnemy.Y] == 'true'):
+            print(Lay.layout[0][testEnemy.X*20 + testEnemy.Y])
             testEnemy.isMade = True
             print(testEnemy.X, testEnemy.Y)
+            print(testEnemy.X*20+testEnemy.Y)
+            
         else:
-            testEnemy.X = random.randrange(20)
-            testEnemy.Y = random.randrange(20)
+            testEnemy.X = (random.randrange(19) - 5)
+            testEnemy.Y = (random.randrange(19) - 5)
     else:
-        testEnemy.X = random.randrange(20)
-        testEnemy.Y = random.randrange(20)
-    
+        testEnemy.X = (random.randrange(19) - 5)
+        testEnemy.Y = (random.randrange(19) - 5)
 
-class PlayMode:
-    inPlayMode = False
-    groundIs = [0, 0]
+while(playerOver.isMade == False):
+    if(PlayMode.groundIs[0]*20 + PlayMode.groundIs[1] + 85 < 400):
+        if(Lay.layout[0][PlayMode.groundIs[0]*20 + PlayMode.groundIs[1] + 85] == 'true'):
+            print(Lay.layout[0][PlayMode.groundIs[0]*20 + PlayMode.groundIs[1] + 85])
+            playerOver.isMade = True
+            
+        else:
+            PlayMode.groundIs[0] = (random.randrange(19) - 5)
+            PlayMode.groundIs[1] = (random.randrange(19) - 5)
+    else:
+        PlayMode.groundIs[0] = (random.randrange(19) - 5)
+        PlayMode.groundIs[1] = (random.randrange(19) - 5)
 
 #defs are here
 def LoadSprites(main, enemy, ground, OX, OY, EX, EY, layout, groundIs):
@@ -85,16 +103,21 @@ while(running):
             if(event.type == pygame.KEYDOWN):
                 if(PlayMode.groundIs[1] > -6):
                     if(event.key == pygame.K_w):
+                        if(Lay.layout[0][PlayMode.groundIs[1] * 20 + PlayMode.groundIs[0] + 64] == 'true'):
                             PlayMode.groundIs[1] -= 1
-                if(PlayMode.groundIs[1] < 16):
+                if(PlayMode.groundIs[1] < 15):
                     if(event.key == pygame.K_s):
+                        if(Lay.layout[0][PlayMode.groundIs[1] * 20 + PlayMode.groundIs[0] + 104] == 'true'):
                             PlayMode.groundIs[1] += 1
                 if(PlayMode.groundIs[0] > -6):
                     if(event.key == pygame.K_a):
+                        if(Lay.layout[0][PlayMode.groundIs[1] * 20 + PlayMode.groundIs[0] + 83] == 'true'):
                             PlayMode.groundIs[0] -= 1
                 if(PlayMode.groundIs[0] < 16):
                     if(event.key == pygame.K_d):
+                        if(Lay.layout[0][PlayMode.groundIs[1] * 20 + PlayMode.groundIs[0] + 85] == 'true'):
                             PlayMode.groundIs[0] += 1
+                            
                 print(PlayMode.groundIs[0], PlayMode.groundIs[1], testEnemy.X, testEnemy.Y)
     if(PlayMode.groundIs[0] == testEnemy.X - 5 and PlayMode.groundIs[1] == testEnemy.Y - 5):
         print('why')
